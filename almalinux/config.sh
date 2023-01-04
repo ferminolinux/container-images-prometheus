d5039db8b203b27523ffdcb62327ef0117ea74d0
Marcus Fermino <marcusfermino.linux@gmail.com>
2023-01-04 11:43:24 -0300

    Removidas camadas desnecessárias

--- almalinux/config.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-URL='https://github.com/prometheus/prometheus/releases/download/v2.41.0/prometheus-2.41.0.linux-amd64.tar.gz'
-curl -fsSLo prometheus.tar.gz $URL
-
-mkdir prometheus
-tar -C prometheus --strip-components 1 -xf prometheus.tar.gz
-rm prometheus.tar.gz
-
